119a734b3a8199ac857319f76acfbae8f20d2a5d
sdcmc <sudachimochi@gmail.com>
2019-06-13 23:19:51 +0900

    GitHubにソース移行

---
+++ js/background.js
@@ -0,0 +1,9 @@
+﻿
+// リクエスト待機
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.method == "getLocalStorage")
+      sendResponse({data: localStorage[request.key]});
+    else
+      sendResponse({}); // snub them.
+});
+
